76ac8ff9576bfbee6d77273ed2df62cc0537b33a
alexlsilva7 <alex.lsilva154@gmail.com>
2022-09-19 18:14:45 -0300

    modificado auth page view para os diferentes tipos de login

--- app/controllers/auth_page_controller.rb
+++ app/controllers/auth_page_controller.rb
@@ -1,4 +1,10 @@
 class AuthPageController < ApplicationController
   def index
+    if admin_signed_in?
+      redirect_to home_page_index_path
+    end
+    if paciente_signed_in?
+      redirect_to home_page_index_path
+    end
   end
 end
