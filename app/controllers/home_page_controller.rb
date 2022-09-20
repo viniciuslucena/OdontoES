95f6a5f5d7a906968a2d054c19fa6deb8dcc23a4
alexlsilva7 <alex.lsilva154@gmail.com>
2022-09-19 21:52:34 -0300

    redirecionamento para tela de login se não logado

--- app/controllers/home_page_controller.rb
+++ app/controllers/home_page_controller.rb
@@ -1,4 +1,15 @@
 class HomePageController < ApplicationController
+  before_action :require_login
   def index
+  end
+
+  def require_login
+    unless admin_signed_in?
+      redirect_to auth_page_index_path
+    else
+      unless paciente_signed_in?
+        redirect_to auth_page_index_path
+      end
+    end
   end
 end
