907c99f7c178965119482f73a1dd206094fbdcba
Vinicius Lucena <jviniiciuslucena@gmail.com>
2022-09-19 22:58:08 -0300

    Atualizacao do ambiente local com o repositorio remoto

--- test/models/paciente_test.rb
+++ test/models/paciente_test.rb
@@ -2,18 +2,17 @@ require "test_helper"
 
 class PacienteTest < ActiveSupport::TestCase
   test "criar paciente valido" do
-    paciente = Paciente.new(nome:"Daniel", data_nascimento:"17-02-2001", cpf:"013.862.694-41", email:"danielduarte@gmail.com")
+    paciente = Paciente.new(nome:"John Doe", data_nascimento:"08-07-1995", cpf:"070.126.704-60", email:"johndoe@gmail.com")
     assert paciente.save
   end
 
   test "criar paciente com nome invalido" do
-    paciente = Paciente.new(nome:"", data_nascimento:"17-02-2001", cpf:"013.862.694-41", email:"danielduartefilho.df@gmail.com")
+    paciente = Paciente.new(nome:"", data_nascimento:"08-07-1995", cpf:"070.126.704-60", email:"johndoe@gmail.com")
     assert_not paciente.save
   end
 
   test "criar paciente com data de nascimento invalida" do
-    paciente = Paciente.new(nome:"Daniel", data_nascimento:"17-02-2025", cpf:"013.862.694-41", email:"danielduartefilho.df@gmail.com")
+    paciente = Paciente.new(nome:"John Doe", data_nascimento:"08-07-1995", cpf:"070.126.704-60", email:"johndoe@gmail.com")
     assert_not paciente.save
   end
-
 end
