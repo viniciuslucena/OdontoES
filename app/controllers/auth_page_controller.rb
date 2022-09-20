18ac66629fc9509499144d96913b089abec1ee34
alexlsilva7 <alex.lsilva154@gmail.com>
2022-09-19 21:53:30 -0300

    correção de multiplas chamadas do redirect_to em auth page

--- app/controllers/auth_page_controller.rb
+++ app/controllers/auth_page_controller.rb
@@ -2,8 +2,7 @@ class AuthPageController < ApplicationController
   def index
     if admin_signed_in?
       redirect_to home_page_index_path
-    end
-    if paciente_signed_in?
+    elsif paciente_signed_in?
       redirect_to home_page_index_path
     end
   end
