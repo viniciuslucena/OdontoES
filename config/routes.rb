7b1fd2259889e04ef80b480cb8c6709d46264352
alexlsilva7 <alex.lsilva154@gmail.com>
2022-09-19 21:51:41 -0300

    atualização de rotas

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,18 @@ Rails.application.routes.draw do
   get 'auth_page/index'
   devise_for :admins
   get 'home_page/index'
-  resources :pacientes
+
+  devise_scope :admin do
+    authenticated :admin do
+      get "/admins/sign_out" => 'devise/sessions#destroy'
+    end
+
+  end
+  devise_scope :paciente do
+    authenticated :paciente do
+      get "/pacientes/sign_out" => "devise/sessions#destroy"
+    end
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
