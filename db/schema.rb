4889a817771dcfe05f462bf8d2d1c8a979b87b31
alexlsilva7 <alex.lsilva154@gmail.com>
2022-09-20 10:52:00 -0300

    adicionado nome em administrador

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_19_195013) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_20_133840) do
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -19,6 +19,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_19_195013) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "nome"
     t.index ["email"], name: "index_admins_on_email", unique: true
     t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
   end
