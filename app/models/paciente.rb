f1b66f6daf72f3623a3b43e46018b1d21147ab6e
Vinicius Lucena <jviniiciuslucena@gmail.com>
2022-09-19 22:57:26 -0300

    feat: Validates de paciente finalizados

--- app/models/paciente.rb
+++ app/models/paciente.rb
@@ -1,2 +1,20 @@
+require "cpf_cnpj"
+
 class Paciente < ApplicationRecord
+  validates :nome, presence: true, length: {in: 5..40}
+  validate :data_nascimento_valida
+  validate :cpf_valido
+  validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP }
+
+  def data_nascimento_valida
+    if data_nascimento.present? && data_nascimento > Date.today
+      errors.add(:data, 'nao pode ser no futuro')
+    end
+  end
+
+  def cpf_valido
+    if cpf.present? && !CPF.valid?(cpf)
+      errors.add(:cpf, 'formato ou numero errado')
+    end
+  end
 end
