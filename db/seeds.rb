c76c37d969aa76af76391782b218cd83ef018684
alexlsilva7 <alex.lsilva154@gmail.com>
2022-09-20 21:29:31 -0300

    adicionado admin padrão

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Admin.create(:nome => "Admin Default", :email => 'admin@admin.com', :password => '987654321', :password_confirmation => '987654321')
