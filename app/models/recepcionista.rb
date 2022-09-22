25911ed1dec37a57409e350a25c0dc8eda07b8da
Antonio Bezerra <antonio.bezerra1414@gmail.com>
2022-09-22 11:49:34 -0300

    Adição dos validates no Model de Recepcionista

--- app/models/recepcionista.rb
+++ app/models/recepcionista.rb
@@ -3,4 +3,14 @@ class Recepcionista < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nome, presence: true, length: {in: 5..40}
+  validate :cpf_valido
+  validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP }
+  def cpf_valido
+    if cpf.present? && !CPF.valid?(cpf)
+      errors.add(:cpf, 'formato ou numero errado')
+    end
+  end
+
 end
