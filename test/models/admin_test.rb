aef3a82df49ae81fad26c5e2fe3eb2e44ddfc91f
alexlsilva7 <alex.lsilva154@gmail.com>
2022-09-23 15:12:56 -0300

    implementado 3 testes de model em Admin

--- test/models/admin_test.rb
+++ test/models/admin_test.rb
@@ -1,7 +1,18 @@
 require "test_helper"
 
 class AdminTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "criar admin valido" do
+    admin = Admin.new(nome:"Alex Lopes", email:"alex@gmail.com" ,password:"123456")
+    assert admin.save
+  end
+
+  test "criar admin com nome invalido" do
+    admin = Admin.new(nome:"", email:"alex@gmail.com" ,password:"123456")
+    assert_not admin.save
+  end
+
+  test "criar admin com email invalido" do
+    admin = Admin.new(nome:"Alex Lopes", email:"email" ,password:"123456")
+    assert_not admin.save
+  end
 end
