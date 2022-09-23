29a5047da2417a556d6eaa717075679adb262213
Vinicius Lucena <jviniiciuslucena@gmail.com>
2022-09-23 12:39:03 -0300

    validation da data de nascimento para paciente

--- app/models/paciente.rb
+++ app/models/paciente.rb
@@ -8,9 +8,15 @@ class Paciente < ApplicationRecord
   validates :nome, presence: true, length: {in: 5..40}
   validate :cpf_valido
   validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validate :nascimentoEhValido
   def cpf_valido
     if cpf.present? && !CPF.valid?(cpf)
       errors.add(:cpf, 'formato ou numero errado')
+    end
+  end
+  def nascimentoEhValido
+    if(data_nascimento.present? && data_nascimento > Date.today)
+      errors.add(:data_nascimento, '- Insira uma data anterior a atual')
     end
   end
 end
