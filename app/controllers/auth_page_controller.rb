4c669ec38cb213278bca5047f8cc2d2370b18a5a
Antonio Bezerra <antonio.bezerra1414@gmail.com>
2022-10-09 18:29:51 -0300

    Refactoring - Extract Function - auth_page Controller

--- app/controllers/auth_page_controller.rb
+++ app/controllers/auth_page_controller.rb
@@ -1,11 +1,20 @@
 class AuthPageController < ApplicationController
   def index
-    if admin_signed_in?
-      redirect_to home_page_index_path
-    elsif paciente_signed_in?
-      redirect_to home_page_index_path
-    elsif recepcionista_signed_in?
+    if verify_if_logged_in
       redirect_to home_page_index_path
     end
+  end
+
+  private
+
+  def verify_if_logged_in
+    if admin_signed_in?
+      return true
+    elsif paciente_signed_in?
+      return true
+    elsif recepcionista_signed_in?
+      return true
+    end
+
   end
 end
